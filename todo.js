59c248008008b5b78af06647cda59a5e7854b847
haznaarustom <115489450+haznaarustom@users.noreply.github.com>
2023-05-30 20:44:24 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,29 @@
+import { Checkbox, IconButton, ListItem } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import React from "react";
+
+function Todo({ todo,toggleComplete, removeTodo }) {
+    function handleCheckboxClick() {
+        toggleComplete(todo.id);
+    }
+
+    function handleRemoveClick() {
+        removeTodo(todo.id);
+    }
+
+    return (
+    <ListItem style={{ display: "flex"}}>
+        <Checkbox
+            checked={todo.completed}
+
+            onClick={handleCheckboxClick}
+        />
+       <p>{todo.task}</p>
+        <IconButton onClick={handleRemoveClick}>
+            <CloseIcon />
+        </IconButton>
+    </ListItem>
+    );
+}
+
+export default Todo;
